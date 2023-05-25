3639a5e9c2dabb460886bc51b8f34bbd0287668d
JonathanCamachoRonquillo <camachoj19@icloud.com>
2023-05-25 09:40:55 -0600

    Creacion Metodo para alimentar tabla DB

--- MagicVilla_API/Datos/ApplicationDBContext.cs
+++ MagicVilla_API/Datos/ApplicationDBContext.cs
@@ -12,6 +12,37 @@ namespace MagicVilla_API.Datos
         //Crear modelos de tablas en la base de datos
         public DbSet<Villa> Villas { get; set; }//Despues crear conexion en 'appsetting.json'
 
-
+        //Método para insertar datos a tabla Villas
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Villa>().HasData(
+                    new Villa()
+                    {
+                        Id = 1,
+                        Nombre = "Villa Real",
+                        Detalle = "Villa del Real Madrid",
+                        ImagenURL = "",
+                        Ocupantes = 100,
+                        MetrosCuadrados = 2000,
+                        Tarifa = 220d,
+                        Amenidad = "",
+                        FechaCreacion = DateTime.Now,
+                        FechaActualizacion = DateTime.Now
+                    },
+                    new Villa()
+                    {
+                        Id = 2,
+                        Nombre = "Villa Rica",
+                        Detalle = "Villa con buena vista",
+                        ImagenURL = "",
+                        Ocupantes = 10,
+                        MetrosCuadrados = 200,
+                        Tarifa = 120d,
+                        Amenidad = "",
+                        FechaCreacion = DateTime.Now,
+                        FechaActualizacion = DateTime.Now
+                    }
+            );
+        }
     }
 }
---
+++ MagicVilla_API/Migrations/20230525153128_AlimentarTablaVilla.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MagicVilla_API.Migrations
+{
+    public partial class AlimentarTablaVilla : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Villas",
+                columns: new[] { "Id", "Amenidad", "Detalle", "FechaActualizacion", "FechaCreacion", "ImagenURL", "MetrosCuadrados", "Nombre", "Ocupantes", "Tarifa" },
+                values: new object[] { 1, "", "Villa del Real Madrid", new DateTime(2023, 5, 25, 9, 31, 28, 169, DateTimeKind.Local).AddTicks(2913), new DateTime(2023, 5, 25, 9, 31, 28, 169, DateTimeKind.Local).AddTicks(2895), "", 2000, "Villa Real", 100, 220.0 });
+
+            migrationBuilder.InsertData(
+                table: "Villas",
+                columns: new[] { "Id", "Amenidad", "Detalle", "FechaActualizacion", "FechaCreacion", "ImagenURL", "MetrosCuadrados", "Nombre", "Ocupantes", "Tarifa" },
+                values: new object[] { 2, "", "Villa con buena vista", new DateTime(2023, 5, 25, 9, 31, 28, 169, DateTimeKind.Local).AddTicks(2920), new DateTime(2023, 5, 25, 9, 31, 28, 169, DateTimeKind.Local).AddTicks(2918), "", 200, "Villa Rica", 10, 120.0 });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Villas",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Villas",
+                keyColumn: "Id",
+                keyValue: 2);
+        }
+    }
+}
